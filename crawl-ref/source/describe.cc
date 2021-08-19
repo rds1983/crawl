e1da59c9f0c04099dbd743a689c243611d3c5e87
Edgar A. Bering IV <trizor@gmail.com>
2021-08-19 16:09:02 +0300

    feat: buff iron golems (amcnicky)
    Iron Golems are slow, and slow monsters are often sad. Dis has iron
    giants, which can throw them into range, which makes them more exciting.
    Except they're also not that strong, and attack slowly to boot.
    
    To make them a better Dis monster this gives them a big buff to their AC
    and HP, the idea being that they are avoidable but will block a player's
    manouverability. To make their obstruction to manouvering even more
    pointed, they get buffed attack and a second attack, and attack at
    normal speed. The normal speed attack ensures that (depending on monster
    list sequencing) they get a good chance to swing at the player after
    being thrown. Big big big ruk.

--- crawl-ref/source/describe.cc
+++ crawl-ref/source/describe.cc
@@ -3969,7 +3969,6 @@ static string _monster_spells_description(const monster_info& mi)
 static const char *_speed_description(int speed)
 {
     // These thresholds correspond to the player mutations for fast and slow.
-    ASSERT(speed != 10);
     if (speed < 7)
         return "extremely slowly";
     else if (speed < 8)
@@ -3983,7 +3982,7 @@ static const char *_speed_description(int speed)
     else if (speed > 10)
         return "quickly";
 
-    return "buggily";
+    return "normally";
 }
 
 static void _add_energy_to_string(int speed, int energy, string what,
@@ -3993,9 +3992,9 @@ static void _add_energy_to_string(int speed, int energy, string what,
         return;
 
     const int act_speed = (speed * 10) / energy;
-    if (act_speed > 10)
+    if (act_speed > 10 || (act_speed == 10 && speed < 10))
         fast.push_back(what + " " + _speed_description(act_speed));
-    if (act_speed < 10)
+    if (act_speed < 10 || (act_speed == 10 && speed > 10))
         slow.push_back(what + " " + _speed_description(act_speed));
 }
 
