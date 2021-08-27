ef109bb543307ea4aee4181367a467867de97ce2
Edgar A. Bering IV <trizor@gmail.com>
2021-08-27 11:46:02 +0300

    feat: new monster, wendigo
    Wendigo are a new Cocytus monster. They're somewhat flimsy and act as
    threat multipliers, with stunning burst and a new spell seracfall.
    Seracfall turns a simulacrum in view into a very high power (3d43)
    iceblast. Cocytus will have plenty of simulacra around as part of the
    overhaul, but many simulacra are slow. The wendigo "help" the simulacra
    along in a couple of ways. The targeting also means that a player facing
    a wendigo plus simulacra will have to manage LOF from several
    directions as the simulacra advance.

--- crawl-ref/source/monster-type.h
+++ crawl-ref/source/monster-type.h
@@ -659,6 +659,7 @@ enum monster_type                      // env.mons[].type
 #else
     MONS_DRYAD,
     MONS_ELEIONOMA,
+    MONS_WENDIGO,
     MONS_SNAPLASHER_VINE,
     MONS_SNAPLASHER_VINE_SEGMENT,
 #endif
@@ -1133,6 +1134,7 @@ enum monster_type                      // env.mons[].type
     MONS_CRYSTAL_ECHIDNA,
     MONS_PUTRID_MOUTH,
     MONS_TAINTED_LEVIATHAN,
+    MONS_WENDIGO,
 #endif
 
     NUM_MONSTERS,               // used for polymorph
